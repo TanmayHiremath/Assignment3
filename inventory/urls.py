622374144ba7f79ee9e95bf53dbac9edbc338b8a
TanmayHiremath <tanmay.hiremath@gmail.com>
2020-04-19 17:57:07 +0530

    .

---
+++ inventory/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from inventory import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+
+
+    path('machine/add',views.MachineCreate.as_view(),name='add-machine'),
+
+     
+    path('machine/<int:pk>/edit/',views.MachineUpdate.as_view(),name='edit-machine'),
+
+
+    path('machine/<int:pk>/delete/',views.MachineDelete.as_view(),name='delete-machine'),
+    
+
+    path('login/',views.loginPage,name='login'),
+
+
+    path('register/',views.registerPage,name='register'),
+
+    
+    path('logout/', views.logoutUser, name="logout"),
+
+
+]
